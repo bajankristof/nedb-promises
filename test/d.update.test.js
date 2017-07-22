5fb036d1488c48a21f5c333fa93922c245cf67f3
Peter Kamps <npeterkamps@gmail.com>
2017-07-22 20:28:00 +0200

    Match NeDB's update callback signature by resolving an array when upsert or returnUpdatedDocs is enabled inside options.

---
+++ test/d.update.test.js
@@ -0,0 +1,82 @@
+const {expect} = require('chai'),
+  Datastore = require('../lib/Datastore');
+
+describe('Update', () => {
+  let documents = [
+    { name: 'first document' },
+    { name: 'second document' },
+    { name: 'third document' }
+  ];
+
+  describe(`single`, () => {
+    it('should update single document', () => {
+      let db = new Datastore();
+      return db.insert(documents)
+        .then((inserted) => {
+          return db.update(
+            { name: 'first document' },
+            { name: 'updated document' },
+            { multi: false }
+          );
+        })
+        .then((numAffected) => {
+          expect(numAffected).to.equal(1);
+        });
+    });
+  });
+
+  describe(`single affected`, () => {
+    it('should update and return single document', () => {
+      let db = new Datastore();
+      return db.insert(documents)
+        .then((inserted) => {
+          return db.update(
+            { name: 'first document' },
+            { name: 'updated document' },
+            { multi: false, returnUpdatedDocs: true }
+          );
+        })
+        .then(([numAffected, affectedDocument]) => {
+          expect(numAffected).to.equal(1);
+          expect(affectedDocument).to.deep.include({
+            name: 'updated document'
+          });
+        });
+    });
+  });
+
+  describe(`bulk`, () => {
+    it('should update multiple documents', () => {
+      let db = new Datastore();
+      return db.insert(documents)
+        .then((inserted) => {
+          return db.update(
+            { name: { $regex: /document$/ } },
+            { $set: { test: true } },
+            { multi: true }
+          );
+        })
+        .then((numAffected) => {
+          expect(numAffected).to.equal(3);
+        });
+    });
+  });
+
+  describe(`bulk affected`, () => {
+    it('should update and return multiple documents', () => {
+      let db = new Datastore();
+      return db.insert(documents)
+        .then((inserted) => {
+          return db.update(
+            { name: { $regex: /document$/ } },
+            { $set: { test: true } },
+            { multi: true, returnUpdatedDocs: true }
+          );
+        })
+        .then(([numAffected, affectedDocuments]) => {
+          expect(numAffected).to.equal(3);
+          expect(affectedDocuments).to.be.an('array').that.has.lengthOf(3);
+        });
+    });
+  });
+});
